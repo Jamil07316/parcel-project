7ac72312503c361cf557cc073f258a0b173dc5ce
jamil <jamil07316@gmail.com>
2022-12-22 08:03:30 +0600

    added parcel project file

---
+++ parcel.js
@@ -0,0 +1,27 @@
+/*
+    parcel 
+    ===========================
+    start video- 42:00 min
+
+    
+    web link: https://parceljs.org/getting-started/webapp/
+
+step-1:
+    Installation: npm install 
+
+    after Installation, checking command: node -v, npm -v
+
+    change directory command for entry in folder: cd folderName
+
+
+step-2
+    initialize command: npm init
+
+    command: npm install parcel --save-dev 
+
+    ctrl + c - used for parcel shutdown
+
+    
+
+
+*/
